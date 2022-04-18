f21810ec10b485c1d218575102b32f5a4bf5ea56
PetarR21 <petar.r.rakocevic@gmail.com>
2022-04-18 14:53:29 +0200

    Commit 4

--- script.js
+++ script.js
@@ -56,6 +56,17 @@ class ToDoList {
 
         return returnItem;
     }
+
+    updateItem(id, title, details, date, priority) {
+        this.list.forEach((item) => {
+            if (item.id === id) {
+                item.title = title;
+                item.details = details;
+                item.date = date;
+                item.priority = priority;
+            }
+        })
+    }
 }
 
 class UI {
@@ -67,21 +78,6 @@ class UI {
 
         const StoredItems = [
             new ToDoItem('Item1', 'Details1', '2022-04-15', 'high'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
-            new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
             new ToDoItem('Item2', 'Details3', '2022-06-09', 'low'),
         ]
 
@@ -132,9 +128,32 @@ class UI {
         document.querySelector('.details-overlay-details .value').textContent = item.details;
     }
 
+    static setEdit(element) {
+        const item = this.list.getElementByID(parseInt(element.dataset.id));
+        const editOverlay = document.querySelector('#edit-overlay');
+        editOverlay.setAttribute('data-editId', element.dataset.id);
+        const titleInput = editOverlay.querySelector('input[type="text"]');
+        const detailsInput = editOverlay.querySelector('textarea');
+        const dateInput = editOverlay.querySelector('input[type="date"]');
+        const priorityInputs = editOverlay.querySelectorAll('input[name="edit-radio"]');
+
+        titleInput.value = item.title;
+        detailsInput.value = item.details;
+        dateInput.value = item.date;
+
+        if (item.priority === 'low') {
+            priorityInputs[0].checked = true;
+        } else if (item.priority === 'mid') {
+            priorityInputs[1].checked = true;
+        } else {
+            priorityInputs[2].checked = true;
+        }
+    }
+
     static addEventListeners(element) {
         const deleteBtn = element.querySelector('.delete-btn');
         const detailsBtn = element.querySelector('.details')
+        const editBtn = element.querySelector('.edit-btn')
 
         deleteBtn.addEventListener('click', () => {
             console.log(this.list);
@@ -147,6 +166,12 @@ class UI {
             this.setDetails(element);
             this.openDetails();
         })
+
+        editBtn.addEventListener('click', () => {
+            this.openEdit();
+            this.setEdit(element);
+        })
+
     }
 
     static openOverlay() {
@@ -203,6 +228,28 @@ class UI {
         this.toggleBlur();
         this.toggleDetails();
     }
+
+    static toggleEdit() {
+        document.querySelector('#edit-overlay').classList.toggle('hidden');
+    }
+
+    static openEdit() {
+        if (document.querySelector('#edit-overlay').classList.contains('hidden')) {
+            this.toggleBlur();
+            this.toggleEdit();
+        }
+    }
+
+    static closeEdit() {
+        this.toggleBlur();
+        this.toggleEdit();
+    }
+
+    static updateElement(id, newTitle, newDetails, newDate, newPriority) {
+        console.log(this.list.getList());
+        this.list.updateItem(id, newTitle, newDetails, newDate, newPriority);
+        console.log(this.list.getList());
+    }
 }
 
 //Event: Display Books
@@ -221,6 +268,23 @@ document.querySelector('#close-overlay-btn').addEventListener('click', () => {
 //Event: Close details overlay
 document.querySelector('#closeDetailsOverlay').addEventListener('click', () => {
     UI.closeDetails();
+})
+
+//Event: Close event overlay
+document.querySelector('#edit-close-btn').addEventListener('click', () => {
+    UI.closeEdit();
+})
+
+//Event: Click on confirm edit button
+document.querySelector('#edit-overlay').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = parseInt(e.target.dataset.editid);
+    const title = document.querySelector('#edit-overlay input[type="text"]').value;
+    const details = document.querySelector('#edit-overlay textarea').value;
+    const date = document.querySelector('#edit-overlay input[type="date"]').value;
+    const priority = document.querySelector('#edit-overlay input[type="radio"]:checked').dataset.value;
+
+    UI.updateElement(id, title, details, date, priority);
 })
 
 //Event: Submit overlay form and add item to list
